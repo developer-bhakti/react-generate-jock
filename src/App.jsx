99e9bf69b0d0cd71610756bd9c3af6fcd564109b
purnale <bhaktipurnale89@gmail.com>
2025-03-07 11:04:24 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,83 @@
-// import React, { useState } from "react";
-// import Card from "./components/Card";
+// // import React, { useState } from "react";
+// // import Card from "./components/Card";
 
-// const App = () => {
+// // const App = () => {
+// //   const [jock, setJocks] = useState([]);
+
+// //  console.log(jock);
+
+// //  const generateJock = (e) => {
+// //   e.preventDefault();
+// //   setJocks();
+// //  }
+
+// //   return (
+//     // <div className="flex justify-center item-center">
+//     //   <div className="flex flex-col justify-center item-center h-screen">
+//     //     <input
+//     //       className="border-2 p-10 mb-5 text-center rounded-sm"
+//     //       type="text"
+//     //       placeholder="Here is the jock"
+//     //       value={jock}
+//     //     />
+//     //     <button onClick={generateJock} className="border-2 py-2 px-4 hover:bg-black hover:text-white rounded-2xl">
+//     //       Generate Jock
+//     //     </button>
+//     //   </div>
+//     // </div>
+// //   );
+// // };
+// // <Card />;
+
+// // export default App;
+
+// import React from 'react'
+// import Card from './components/Card'
+// import { useState } from "react";
+
+// function App () {
 //   const [jock, setJocks] = useState([]);
 
-//  console.log(jock);
-
-//  const generateJock = (e) => {
-//   e.preventDefault();
-//   setJocks();
-//  }
-
+//   const generateJock = (e)=> {
+//     e.preventDefault();
+//     console.log(jock)
+//   }
 //   return (
-    // <div className="flex justify-center item-center">
-    //   <div className="flex flex-col justify-center item-center h-screen">
-    //     <input
-    //       className="border-2 p-10 mb-5 text-center rounded-sm"
-    //       type="text"
-    //       placeholder="Here is the jock"
-    //       value={jock}
-    //     />
-    //     <button onClick={generateJock} className="border-2 py-2 px-4 hover:bg-black hover:text-white rounded-2xl">
-    //       Generate Jock
-    //     </button>
-    //   </div>
-    // </div>
+//     <>
+//     <div className="flex justify-center item-center">
+//       <div className="flex flex-col justify-center item-center h-screen">
+//        {/* input box q liya tune ?oh empty box nhi na ban raha tha isiliye tu lai unxpected hai bhendi  */}
+//         <input
+//           className="border-2 p-10 mb-5 text-center rounded-sm"
+//           type="text"
+//           placeholder="Here is the jock"
+//           value={jock}
+//         />
+//         <button onClick={generateJock} className="border-2 py-2 px-4 hover:bg-black hover:text-white rounded-2xl">
+//           Generate Jock
+//         </button>
+//       </div>
+//       <div>
+//         {jock.map(setJocks)}
+//       </div>
+//     </div>
+//     <Card/>
+//     </>
 //   );
-// };
-// <Card />;
+// }
 
 // export default App;
 
 import React from 'react'
 import Card from './components/Card'
-import { useState } from "react";
 
-function App () {
-  const [jock, setJocks] = useState([]);
-
-  const generateJock = (e)=> {
-    e.preventDefault();
-    console.log(jock);
-  }
+const App = () => {
   return (
-    <>
-    <div className="flex justify-center item-center">
-      <div className="flex flex-col justify-center item-center h-screen">
-        <input
-          className="border-2 p-10 mb-5 text-center rounded-sm"
-          type="text"
-          placeholder="Here is the jock"
-          value={jock}
-        />
-        <button onClick={generateJock} className="border-2 py-2 px-4 hover:bg-black hover:text-white rounded-2xl">
-          Generate Jock
-        </button>
-      </div>
-      <div>
-        {jock.map(setJocks)}
-      </div>
+    <div>
+
+      <Card />
     </div>
-    <Card/>
-    </>
-  );
+  )
 }
 
-export default App;
+export default App
